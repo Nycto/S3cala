3bb5fedf09b35c5033d4cff3024d8454a2f845bf
Nycto <James@RoundEights.com>
2013-02-13 20:23:27 -0800

    Add support for fetching keys

--- src/main/scala/s3cala/S3.scala
+++ src/main/scala/s3cala/S3.scala
@@ -1,9 +1,12 @@
 package com.roundeights.s3cala
 
-import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.services.s3.transfer.TransferManager
+import com.amazonaws.services.s3.model.{ProgressListener, ProgressEvent}
 import com.amazonaws.auth.{AWSCredentials, BasicAWSCredentials}
 
-import java.io.InputStream
+import java.io.{InputStream, File, FileInputStream}
+
+import scala.concurrent.{Promise, Future, ExecutionContext}
 
 /**
  * S3 Companion
@@ -11,11 +14,11 @@ import java.io.InputStream
 object S3 {
 
     /** Builds a new instance */
-    def apply( client: AmazonS3Client ): S3 = new S3( client )
+    def apply( client: TransferManager ): S3 = new S3( client )
 
     /** Builds an instance from a set of AWS credentials */
     def apply( credentials: AWSCredentials ): S3
-        = apply( new AmazonS3Client(credentials) )
+        = apply( new TransferManager(credentials) )
 
     /** Builds an instance from an Access key and a Secret Key */
     def apply( accessKey: String, secretKey: String ): S3
@@ -26,15 +29,52 @@ object S3 {
 /**
  * S3 Client wrapper
  */
-class S3 ( private val client: AmazonS3Client ) {
+class S3 ( private val client: TransferManager ) {
+
+    /** Builds a bucket specific client */
+    def bucket ( bucket: String ) = new Bucket( bucket )
+
+    /** Closes this connection */
+    def close: Unit = client.shutdownNow
 
     /**
      * A specific S3 bucket
      */
-    class Bucket ( private val bucket: String )
+    class Bucket ( private val bucket: String ) {
 
-    /** Builds a bucket specific client */
-    def bucket ( bucket: String ) = new Bucket( bucket )
+        /** Downloads a key into a file */
+        def get ( key: String, file: File ): Future[Unit] = {
+            val result = Promise[Unit]
+
+            val upload = client.download( bucket, key, file )
+
+            upload.addProgressListener(new ProgressListener {
+                override def progressChanged ( event: ProgressEvent ): Unit = {
+                    event.getEventCode match {
+                        case ProgressEvent.FAILED_EVENT_CODE
+                            => result.failure( upload.waitForException )
+                        case ProgressEvent.CANCELED_EVENT_CODE
+                            => result.failure( new Exception )
+                        case ProgressEvent.COMPLETED_EVENT_CODE
+                            => result.success( Unit )
+                        case _ => ()
+                    }
+                }
+            })
+
+            result.future
+        }
+
+        /** Downloads a key */
+        def get
+            ( key: String )
+            ( implicit context: ExecutionContext )
+        : Future[InputStream] = {
+            val file = File.createTempFile("S3-",".tmp")
+            get( key, file ).map( _ => new FileInputStream(file) )
+        }
+
+    }
 
 }
 
